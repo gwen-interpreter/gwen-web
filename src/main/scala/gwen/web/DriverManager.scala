388deb63b99ee3f4cfc88dff1e631a4dbd889b07
Branko Juric <branko.juric@gmail.com>
2015-03-24 09:46:27 +1100

    split remote/local driver
    also apply global settings to remote driver

--- src/main/scala/gwen/web/DriverManager.scala
+++ src/main/scala/gwen/web/DriverManager.scala
@@ -52,50 +52,50 @@ trait DriverManager extends LazyLogging {
   }
   
   /** Loads the selenium webdriver. */
-  private[web] def loadWebDriver: WebDriver = {
-    val driverName = WebSettings.`gwen.web.browser` tap { browser =>
-      logger.info(s"Loading $browser web driver")
+  private[web] def loadWebDriver: WebDriver = withGlobalSettings {
+    val driverName = WebSettings.`gwen.web.browser`.toLowerCase
+    logger.info(s"Loading $driverName web driver")
+    WebSettings.`gwen.web.remote.url` match {
+    	case Some(addr) => remoteDriver(driverName, addr)
+      case None => localDriver(driverName)
     }
+  }
+  
+  private def remoteDriver(driverName: String, addr: String): WebDriver = {
+    val capabilities = driverName match {
+      case "firefox" => 
+        DesiredCapabilities.firefox tap { capabilities =>
+          capabilities.setCapability(FirefoxDriver.PROFILE, firefoxProfile)
+        }
+      case "chrome" => DesiredCapabilities.chrome tap { capabilities =>
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions)
+      }
+    }
+    capabilities.setJavascriptEnabled(true)
 
-    val `gwen.web.remote.url` = WebSettings.`gwen.web.remote.url`
-    (`gwen.web.remote.url` match {
-    	case Some(addr) => {
-    	    val capabilities = driverName.toLowerCase() match {
-    	      case "firefox" => DesiredCapabilities.firefox tap { capabilities =>
-    	        capabilities.setCapability(FirefoxDriver.PROFILE, firefoxProfile)
-    	      }
-    	      case "chrome" => DesiredCapabilities.chrome tap { capabilities =>
-    	        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions)
-    	      }
-    	    }
-    		capabilities.setJavascriptEnabled(true)
-
-    		//TODO 
-    		//profile override if the useragent is set.
-    		//set the untrusted certificates
-    		//etc..
-    		//mock the drivers, write tests
-	        //capabilities.setCapability("record-video", true)
-	        //capabilities.setCapability("build", sys.env("TRAVIS_BUILD_NUMBER"))
-	        //capabilities.setCapability("tunnel-identifier", sys.env("TRAVIS_JOB_NUMBER"))
-	        //val username = sys.env("SAUCE_USERNAME")
-	        //val accessKey = sys.env("SAUCE_ACCESS_KEY")
-	        // val hubURL = new URL(s"http://$username:$accessKey@localhost:4445/wd/hub")
-	        //val hubURL = new URL(s"http://localhost:44466/wd/hub")
-	        val hubURL = new URL(addr)
-	        val commandExecutor = new HttpCommandExecutor(hubURL)
-	        new RemoteWebDriver(commandExecutor, capabilities)
-    	}
-    	case None =>  (driverName.toLowerCase() match {
-    			case "firefox" => firefox()
-    			case "ie" => ie()
-    			case "chrome" => chrome()
-    			case "safari" => safari()
-    			case _ => sys.error(s"Unsupported webdriver: $driverName")
-    			}) tap { driver =>
-    				applyGlobalSettings(driver)
-    			}
-    	})
+    //TODO
+    //profile override if the useragent is set.
+    //set the untrusted certificates
+    //etc..
+    //mock the drivers, write tests
+    //capabilities.setCapability("record-video", true)
+    //capabilities.setCapability("build", sys.env("TRAVIS_BUILD_NUMBER"))
+    //capabilities.setCapability("tunnel-identifier", sys.env("TRAVIS_JOB_NUMBER"))
+    //val username = sys.env("SAUCE_USERNAME")
+    //val accessKey = sys.env("SAUCE_ACCESS_KEY")
+    // val hubURL = new URL(s"http://$username:$accessKey@localhost:4445/wd/hub")
+    //val hubURL = new URL(s"http://localhost:44466/wd/hub")
+    val hubURL = new URL(addr)
+    val commandExecutor = new HttpCommandExecutor(hubURL)
+    new RemoteWebDriver(commandExecutor, capabilities)
+  }
+  
+  private def localDriver(driverName: String): WebDriver = driverName match {
+    case "firefox" => firefox()
+    case "ie" => ie()
+    case "chrome" => chrome()
+    case "safari" => safari()
+    case _ => sys.error(s"Unsupported webdriver: $driverName")
   }
   
   private def firefoxProfile() : FirefoxProfile = new FirefoxProfile() tap { profile =>
@@ -127,11 +127,12 @@ trait DriverManager extends LazyLogging {
   
   private def safari(): WebDriver = new SafariDriver()
   
-  private def applyGlobalSettings(driver: WebDriver): Unit = {
+  private def withGlobalSettings(driver: WebDriver): WebDriver = {
     driver.manage().timeouts().implicitlyWait(WebSettings.`gwen.web.wait.seconds`, TimeUnit.SECONDS)
     if (WebSettings.`gwen.web.maximize`) {
       driver.manage().window().maximize() 
     }
+    driver
   }
   
 }
