cf7436be2296fb87e4a2914bd8814ded21664816
Branko Juric <branko@Brankos-MacBook-Pro-M1.local>
2022-03-11 09:10:02 +1100

    Fix if conditions match to capture condition after last if keyword

--- src/main/scala/gwen/web/eval/WebEngine.scala
+++ src/main/scala/gwen/web/eval/WebEngine.scala
@@ -79,7 +79,7 @@ class WebEngine extends EvalEngine[WebContext] {
           Some(new ForEachWebElement(doStep, element, SelectorType.parse(selectorType), expression, None, Some(Duration.create(timeout.toLong, TimeUnit.SECONDS)), this))
         case r"""(.+?)$doStep for each (.+?)$element located by (id|name|tag name|tag|css selector|css|xpath|class name|class|link text|partial link text|javascript|js)$selectorType "(.+?)"$expression""" =>
           Some(new ForEachWebElement(doStep, element, SelectorType.parse(selectorType), expression, None, None, this))
-        case r"""(.+?)$doStep for each (.+?)$element in (.+?)$$$iteration""" =>
+        case r"""(.+?)$doStep for each (.+?)$element in (.+?)$iteration""" =>
           Some(new ForEachWebElementInIteration(doStep, element, iteration, this))
         case _ => 
           None
@@ -103,11 +103,11 @@ class WebEngine extends EvalEngine[WebContext] {
         new WaitForText(element, None)
       case r"""I wait for (.+?)$element for (\d+)$seconds second(?:s?)""" =>
         new WaitForElement(element, Some(seconds.toInt))
-      case r"""I wait for (.+?)$$$element""" =>
+      case r"""I wait for (.+?)$element""" =>
         new WaitForElement(element, None)
-      case r"""I wait ([0-9]+?)$duration second(?:s?) when (.+?)$element is (clicked|right clicked|double clicked|submitted|checked|ticked|unchecked|unticked|selected|deselected|typed|entered|tabbed|cleared|moved to)$$$event""" =>
+      case r"""I wait ([0-9]+?)$duration second(?:s?) when (.+?)$element is (clicked|right clicked|double clicked|submitted|checked|ticked|unchecked|unticked|selected|deselected|typed|entered|tabbed|cleared|moved to)$event""" =>
         new WaitForElementOnEvent(element, ElementEvent.valueOf(event), duration.toLong)
-      case r"""I wait until (.+?)$condition when (.+?)$element is (clicked|right clicked|double clicked||submitted|checked|ticked|unchecked|unticked|selected|deselected|typed|entered|tabbed|cleared|moved to)$$$event""" =>
+      case r"""I wait until (.+?)$condition when (.+?)$element is (clicked|right clicked|double clicked||submitted|checked|ticked|unchecked|unticked|selected|deselected|typed|entered|tabbed|cleared|moved to)$event""" =>
         new WaitForConditionOnEvent(element, ElementEvent.valueOf(event), condition)
       case r"""I wait until "(.+?)$javascript" using (.+?)$delayPeriod (second|millisecond)$delayUnit delay and (.+?)$timeoutPeriod (minute|second|millisecond)$timeoutUnit timeout""" => 
         new WaitForCondition(javascript, Some(Duration(delayPeriod.toLong, delayUnit).toMillis), Some(Duration(timeoutPeriod.toLong, timeoutUnit).toSeconds))
@@ -117,7 +117,7 @@ class WebEngine extends EvalEngine[WebContext] {
         new WaitForCondition(javascript, None, Some(Duration(timeoutPeriod.toLong, timeoutUnit).toSeconds))
       case r"""I wait until "(.+?)$javascript"""" =>
         new WaitForCondition(step.orDocString(javascript), None, None)
-      case r"""I wait until (.+?)$element is( not)?$negation (displayed|hidden|checked|ticked|unchecked|unticked|enabled|disabled)$$$state""" =>
+      case r"""I wait until (.+?)$element is( not)?$negation (displayed|hidden|checked|ticked|unchecked|unticked|enabled|disabled)$state""" =>
         new WaitForElementState(element, ElementState.valueOf(state), Option(negation).isDefined)
       case r"""I wait until (.+?)$condition using (.+?)$delayPeriod (second|millisecond)$delayUnit delay and (.+?)$timeoutPeriod (minute|second|millisecond)$timeoutUnit timeout""" =>
         new WaitForBoundCondition(condition, Some(Duration(delayPeriod.toLong, delayUnit).toMillis), Some(Duration(timeoutPeriod.toLong, timeoutUnit).toSeconds))
@@ -125,21 +125,21 @@ class WebEngine extends EvalEngine[WebContext] {
         new WaitForBoundCondition(condition, Some(Duration(delayPeriod.toLong, delayUnit).toMillis), None)
       case r"""I wait until (.+?)$condition using (.+?)$timeoutPeriod (minute|second|millisecond)$timeoutUnit timeout""" =>
         new WaitForBoundCondition(condition, None, Some(Duration(timeoutPeriod.toLong, timeoutUnit).toSeconds))
-      case r"""I wait until (.+?)$$$condition""" =>
+      case r"""I wait until (.+?)$condition""" =>
         new WaitForBoundCondition(condition, None, None)
-      case r"""I am on the (.+?)$$$page""" =>
+      case r"""I am on the (.+?)$page""" =>
         new CreatePageScope(page)
-      case r"""I navigate to the (.+?)$$$page""" =>
+      case r"""I navigate to the (.+?)$page""" =>
         new NavigateToPageInScope(page)
-      case r"""I navigate to "(.+?)"$$$url""" =>
+      case r"""I navigate to "(.+?)"$url""" =>
         new NavigateToUrl(step.orDocString(url))
-      case r"""I scroll to the (top|bottom)$position of (.+?)$$$element""" =>
+      case r"""I scroll to the (top|bottom)$position of (.+?)$element""" =>
         new ScrollToElement(element, ScrollTo.valueOf(position))
-      case r"""the url will be defined by (?:property|setting) "(.+?)"$$$name""" =>
+      case r"""the url will be defined by (?:property|setting) "(.+?)"$name""" =>
         new BindUrl(Settings.get(step.orDocString(name)), None)
-      case r"""the url will be "(.+?)"$$$url""" =>
+      case r"""the url will be "(.+?)"$url""" =>
         new BindUrl(step.orDocString(url), None)
-      case r"""the (.+?)$page url is "(.+?)"$$$url""" =>
+      case r"""the (.+?)$page url is "(.+?)"$url""" =>
         new BindUrl(step.orDocString(url), Some(page))
       case r"""(.+?)$element can be located by (id|name|tag name|tag|css selector|css|xpath|class name|class|link text|partial link text|javascript|js)$selectorType "(.+?)"$expression at index (\d+)$index in (.+?)$container with no (?:timeout|wait)""" =>
         new BindElementLocator(element, SelectorType.parse(selectorType), expression, Some((RelativeSelectorType.in, container, None)), Some(0), Some(index.toInt))
@@ -157,7 +157,7 @@ class WebEngine extends EvalEngine[WebContext] {
         new BindElementLocator(element, SelectorType.parse(selectorType), expression, Some((RelativeSelectorType.near, rElement, Some(pixels.toInt))), Some(0), None)
       case r"""(.+?)$element can be located by (id|name|tag name|tag|css selector|css|xpath|class name|class|link text|partial link text)$selectorType "(.+?)"$expression near and within (\d+)$pixels pixel(?:s?) of (.+?)$rElement with (\d+)$timeout second (?:timeout|wait)""" =>
         new BindElementLocator(element, SelectorType.parse(selectorType), expression, Some((RelativeSelectorType.near, rElement, Some(pixels.toInt))), Some(timeout.toInt), None)
-      case r"""(.+?)$element can be located by (id|name|tag name|tag|css selector|css|xpath|class name|class|link text|partial link text)$selectorType "(.+?)"$expression near and within (\d+)$pixels pixel(?:s?) of (.+?)$$$rElement""" =>
+      case r"""(.+?)$element can be located by (id|name|tag name|tag|css selector|css|xpath|class name|class|link text|partial link text)$selectorType "(.+?)"$expression near and within (\d+)$pixels pixel(?:s?) of (.+?)$rElement""" =>
         new BindElementLocator(element, SelectorType.parse(selectorType), expression, Some((RelativeSelectorType.near, rElement, Some(pixels.toInt))), None, None)
       case r"""(.+?)$element can be located by (id|name|tag name|tag|css selector|css|xpath|class name|class|link text|partial link text)$selectorType "(.+?)"$expression (above|below|near|to left of|to right of)$rSelectorType (.+?)$rElement with no (?:timeout|wait)""" =>
         new BindElementLocator(element, SelectorType.parse(selectorType), expression, Some((RelativeSelectorType.valueOf(rSelectorType), rElement, None)), Some(0), None)
@@ -195,7 +195,7 @@ class WebEngine extends EvalEngine[WebContext] {
         new BindMultipleElementLocators(element, None, None, Some(index.toInt))
       case r"""(.+?)$element can be located by""" if step.hasDualColumnTable =>
         new BindMultipleElementLocators(element, None, None, None)
-      case r"""(.+?)$element can be (clicked|right clicked|double clicked|submitted|checked|ticked|unchecked|unticked|selected|deselected|typed|entered|tabbed|cleared|moved to)$event by (?:javascript|js) "(.+?)"$$$expression""" =>
+      case r"""(.+?)$element can be (clicked|right clicked|double clicked|submitted|checked|ticked|unchecked|unticked|selected|deselected|typed|entered|tabbed|cleared|moved to)$event by (?:javascript|js) "(.+?)"$expression""" =>
         new BindActionHandler(element, ElementEvent.valueOf(event), step.orDocString(expression))
       case r"""the page title should( not)?$negation (be|contain|start with|end with|match regex|match xpath|match json path|match template|match template file)$operator "(.*?)"$expression(?:: )?(".+")?$message""" =>
         new CompareTitle("title", step.orDocString(expression), false, ComparisonOperator.valueOf(operator), Option(negation).isDefined, Option(message))
@@ -211,7 +211,7 @@ class WebEngine extends EvalEngine[WebContext] {
         new CompareValueOrSelectionToValue(element, Option(selection).map(_.trim).map(DropdownSelection.valueOf), step.orDocString(expression), ComparisonOperator.valueOf(operator), Option(negation).isDefined, Option(message))
       case r"""(.+?)$element( text| value)?$selection should( not)?$negation (be|contain|start with|end with|match regex|match xpath|match json path)$operator (.+?)$attribute(?:: )?(".+")?$message""" if attribute != "absent" && !element.matches(".+at (json path|xpath).+") =>
         new CompareValueOrSelectionToBoundValue(element, Option(selection).map(_.trim).map(DropdownSelection.valueOf), attribute, ComparisonOperator.valueOf(operator), Option(negation).isDefined, Option(message))
-      case r"""I capture (.+?)$attribute (?:of|on|in) (.+?)$element by (?:javascript|js) "(.+?)"$$$expression""" =>
+      case r"""I capture (.+?)$attribute (?:of|on|in) (.+?)$element by (?:javascript|js) "(.+?)"$expression""" =>
         new CaptureElementAttribute(element, attribute, step.orDocString(expression))
       case r"""I capture the current URL""" =>
         new CaptureUrl(None)
@@ -227,43 +227,43 @@ class WebEngine extends EvalEngine[WebContext] {
         new CaptureElementScreenshot(element, None)
       case r"""I capture (.+?)$element( value| text)$selection as (.+?)$attribute""" =>
         new CaptureDropdownOrElement(Some(attribute), element, DropdownSelection.valueOf(selection.trim))
-      case r"""I capture (.+?)$element( value| text)$$$selection""" =>
+      case r"""I capture (.+?)$element( value| text)$selection""" =>
         new CaptureDropdownOrElement(None, element, DropdownSelection.valueOf(selection.trim))
       case r"I capture the (alert|confirmation)$name popup message" =>
         new CapturePopupMessage(s"the $name popup message")
       case r"I capture the (?:alert|confirmation) popup message as (.+?)$attribute" =>
         new CapturePopupMessage(attribute)
-      case r"""I drag and drop (.+?)$source to (.+?)$$$target""" =>
+      case r"""I drag and drop (.+?)$source to (.+?)$target""" =>
         new DragAndDrop(source, target)
-      case r"""I clear (.+?)$$$element""" =>
+      case r"""I clear (.+?)$element""" =>
         new ClearElement(element)
-      case r"""I press (enter|tab)$key in (.+?)$$$element""" =>
+      case r"""I press (enter|tab)$key in (.+?)$element""" =>
         new SendKeyToElement(element, key)
       case r"""I send "(.+?)"$keys""" =>
         new SendKeysToBrowser(keys.split("[,\\+]"))
-      case r"""I send "(.+?)"$keys to (.+?)$$$element""" =>
+      case r"""I send "(.+?)"$keys to (.+?)$element""" =>
         new SendKeysToElement(element, keys.split("[,\\+]"))
-      case r"""I (enter|type)$action "(.*?)"$value in (.+?)$$$element""" =>
+      case r"""I (enter|type)$action "(.*?)"$value in (.+?)$element""" =>
         new SendValueToElement(element, value, ElementAction.valueOf(action) == ElementAction.enter)
-      case r"""I (enter|type)$action (.+?)$attribute in (.+?)$$$element""" =>
+      case r"""I (enter|type)$action (.+?)$attribute in (.+?)$element""" =>
         new SendBoundValueToElement(element, attribute, ElementAction.valueOf(action) == ElementAction.enter)
-      case r"""I (select|deselect)$action the (\d+?)$position(?:st|nd|rd|th) option in (.+?)$$$element""" =>
+      case r"""I (select|deselect)$action the (\d+?)$position(?:st|nd|rd|th) option in (.+?)$element""" =>
         new ChangeDropdownSelection(element, DropdownSelection.index, position, false, ElementAction.valueOf(action))
       case r"""I (select|deselect)$action "(.*?)"$value in (.+?)$element by value""" =>
         new ChangeDropdownSelection(element, DropdownSelection.value, value, false, ElementAction.valueOf(action))
-      case r"""I (select|deselect)$action "(.*?)"$value in (.+?)$$$element""" =>
+      case r"""I (select|deselect)$action "(.*?)"$value in (.+?)$element""" =>
         new ChangeDropdownSelection(element, DropdownSelection.text, value, false, ElementAction.valueOf(action))
       case r"""I (select|deselect)$action (.+?)$attribute in (.+?)$element by value""" =>
         new ChangeDropdownSelection(element, DropdownSelection.value, attribute, true, ElementAction.valueOf(action))
-      case r"""I (select|deselect)$action (.+?)$attribute in (.+?)$$$element""" =>
+      case r"""I (select|deselect)$action (.+?)$attribute in (.+?)$element""" =>
         new ChangeDropdownSelection(element, DropdownSelection.text, attribute, true, ElementAction.valueOf(action))
-      case r"""I (click|right click|double click|check|tick|uncheck|untick|move to)$action (.+?)$element of (.+?)$$$context""" =>
+      case r"""I (click|right click|double click|check|tick|uncheck|untick|move to)$action (.+?)$element of (.+?)$context""" =>
         new PerformElementAction(element, ElementAction.valueOf(action), Some(context))
-      case r"""I (click|right click|double click|submit|check|tick|uncheck|untick|move to)$action (.+?)$$$element""" =>
+      case r"""I (click|right click|double click|submit|check|tick|uncheck|untick|move to)$action (.+?)$element""" =>
         new PerformElementAction(element, ElementAction.valueOf(action), None)
-      case r"""I (.+?)$modifiers (click|right click|double click)$clickAction (.+?)$$$element""" =>
+      case r"""I (.+?)$modifiers (click|right click|double click)$clickAction (.+?)$element""" =>
         new HoldAndClick(element, modifiers.split("\\+"), ElementAction.valueOf(clickAction))
-      case r"""I (?:highlight|locate) (.+?)$$$element""" =>
+      case r"""I (?:highlight|locate) (.+?)$element""" =>
         new HighlightElement(element)
       case "I refresh the current page" =>
         new RefreshPage()
@@ -271,7 +271,7 @@ class WebEngine extends EvalEngine[WebContext] {
         new StartBrowserSession("primary")
       case r"I start a new browser (tab|window)$winType" =>
         new SwitchToNewWindow(WindowType.valueOf(winType.toUpperCase))
-      case r"""I start a browser for (.+?)$$$session""" =>
+      case r"""I start a browser for (.+?)$session""" =>
         new StartBrowserSession(session)
       case r"""I should have (\d+?)$count open browser(?:s?)(?:: )?(".+")?$message""" =>
         new AssertBrowserCount(count.toInt, Option(message))
@@ -303,15 +303,15 @@ class WebEngine extends EvalEngine[WebContext] {
         new SwitchToBrowserSession(session)
       case r"I (accept|dismiss)$action the (?:alert|confirmation) popup" =>
         new HandlePopup(PopupAction.valueOf(action))
-      case r"""I resize the window to width (\d+?)$width and height (\d+?)$$$height""" =>
+      case r"""I resize the window to width (\d+?)$width and height (\d+?)$height""" =>
         new ResizeWindow(width.toInt, height.toInt)
       case r"""I maximi(?:z|s)e the window""" =>
         new MaximiseWindow()
-      case r"""I append "(.+?)"$text to (.+?)$$$element""" =>
+      case r"""I append "(.+?)"$text to (.+?)$element""" =>
         new AppendTextToElement(element, text, false)
-      case r"""I append (.+?)$attribute to (.+?)$$$element""" =>
+      case r"""I append (.+?)$attribute to (.+?)$element""" =>
         new AppendTextToElement(element, attribute, true)
-      case r"I insert a new line in (.+?)$$$element" =>
+      case r"I insert a new line in (.+?)$element" =>
         new AppendNewLineToElement(element)
       case _ => 
         super.translateStep(step)
